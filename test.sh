5220aa0a5b57bda16b47c375099eeda843e30f41
mohammad hamraj ahmed <ahmedhamraj12@gmail.com>
2025-04-22 07:00:08 +0530

    added script

---
+++ test.sh
@@ -0,0 +1,22 @@
+mkdir naresh
+cd naresh
+echo hi file1>> f1
+mkdir ahmed
+cd ahmed
+echo hello ahmed >> f2
+yum install java -y
+yum install wget -y
+yum install unzip -y
+yum install vim -y
+yum install epel-release -y
+yum install ansible -y
+yum install git -y
+yum install maven -y
+yum install python -y
+yum install python-pip -y
+yum install tree -y
+yum install java -y
+yum install nginx -y
+ansible --version
+java -version
+echo successfully completed
